6c6cdf9856e0a5479307df23e652c172ab3293a8
christiantripodi <skl11@icloud.com>
2024-05-10 11:18:25 +0200

    commit8

--- javasript/assets/script.js
+++ javasript/assets/script.js
@@ -88,10 +88,31 @@ console.log(cars);
 */
 const justTrims = [];
 
+
+for(i = 0 ; i < cars.length ; i++){
+  justTrims.push(cars[i].trims[0])
+}
+
+console.log(justTrims)
+
+
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+
+for(i = 0 ; i < cars.length ; i++){
+if(cars[i].color.charAt(0) === 'b'){
+  console.log('Fizz')
+}else{
+  console.log('Buzz')
+}
+
+}
+
+
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
